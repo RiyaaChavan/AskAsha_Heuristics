55add2f3f483df76001ab76aece57c1961e6fa13
Vaishnavi S <vaishnavishridhar23@gmail.com>
2025-04-28 16:17:26 +0530

    Update Interview.jsx

--- frontend/src/Components/Interview.jsx
+++ frontend/src/Components/Interview.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { MessageSquare, ArrowLeft, Search } from 'lucide-react';
-// import './interview.css'; // Import the CSS file
+import './interview.css'; // Import the CSS file
 
 export default function Interview() {
   const [currentChat, setCurrentChat] = useState(null);
